a24f016ca8824f710e5472645abcb484382d3b42
ClaraaVargas <claravargasnovo@gmail.com>
2024-06-10 12:08:43 -0300

    Changes to be committed: 	deleted:    env 	modified:   package-lock.json 	modified:   public/img/logoatual.png 	modified:   resources/views/components/application-logo.blade.php 	modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,93 +1,73 @@
 <nav x-data="{ open: false }"  style="background-color: #9dc8ce;">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 lg:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-            
+            <div class="flex" style="width:100%;">
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 lg:-my-px lg:ms-10 lg:flex" style="justify-content: space-between; width: 100%;">
                     <div class="shrink-0 flex items-center" >
                         <a href="{{ route('welcome') }}" class="pr-2">
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                            <x-application-logo/>
                         </a>
                     </div>
-
+                    <div class="hidden space-x-8 lg:-my-px lg:ms-10 lg:flex">
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-white">
                             {{ __('Mural') }}
                         </x-nav-link>
+                        <x-nav-link :href="route('forumdeduvidas')" :active="request()->routeIs('forumdeduvidas')" class="text-white">
+                            {{ __('Fórum de Dúvidas') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('materias')" :active="request()->routeIs('materias')" class="text-white">
+                            {{ __('Disciplinas') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('questoes')" :active="request()->routeIs('questoes')" class="text-white">
+                            {{ __('Questões') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('resumos')" :active="request()->routeIs('resumos')" class="text-white">
+                            {{ __('Resumos') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('informacoes')" :active="request()->routeIs('informacoes')" class="text-white">
+                            {{ __('Informações ') }}
+                        </x-nav-link>
+                    </div>
+                    <!-- Settings Dropdown -->
+                    <div class="hidden lg:flex lg:items-center lg:ms-6">
+                        <x-dropdown  width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover: #9dc8ce hover:#9dc8ce focus:outline-none transition ease-in-out duration-150">
+                                    <div>{{ Auth::user()->name }}</div>
 
-                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('forumdeduvidas')" :active="request()->routeIs('forumdeduvidas')" class="text-white">
-                                {{ __('Fórum de Dúvidas') }}
-                            </x-nav-link>
-                        </div>
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
 
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Perfil') }}
+                                </x-dropdown-link>
 
-                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('materias')" :active="request()->routeIs('materias')" class="text-white">
-                                {{ __('Disciplinas') }}
-                            </x-nav-link>
-                        </div>
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
 
-
-                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('questoes')" :active="request()->routeIs('questoes')" class="text-white">
-                                {{ __('Questões') }}
-                            </x-nav-link>
-                        </div>
-
-
-                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('resumos')" :active="request()->routeIs('resumos')" class="text-white">
-                                {{ __('Resumos') }}
-                            </x-nav-link>
-                        </div>
-
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <x-nav-link :href="route('informacoes')" :active="request()->routeIs('informacoes')" class="text-white">
-                                    {{ __('Informações ') }}
-                                </x-nav-link>
-                            </div>
-                   
+                                    <x-dropdown-link :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Sair') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown  width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover: #9dc8ce hover:#9dc8ce focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Perfil') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Sair') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center lg:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBoxho="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -99,10 +79,40 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
+        <div class="pt-1 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Mural') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-1 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('forumdeduvidas')" :active="request()->routeIs('forumdeduvidas')">
+                {{ __('Fórum de Dúvidas') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-1 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('materias')" :active="request()->routeIs('materias')">
+                {{ __('Disciplinas') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-1 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('questoes')" :active="request()->routeIs('questoes')">
+                {{ __('Questões') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-1 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('resumos')" :active="request()->routeIs('resumos')">
+                {{ __('Resumos') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-1 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('informacoes')" :active="request()->routeIs('informacoes')">
+                {{ __('Informações') }}
             </x-responsive-nav-link>
         </div>
 
