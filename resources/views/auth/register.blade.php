748dd40159605fc318fc84722f742159258ac23c
SofiaAyumi <sofiayumi17@gmail.com>
2024-07-26 09:30:30 -0300

    alteração front login, cadastro, cabeçalho

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+            <x-text-input style="background-color: #F4F4F4;" id="name" class="focus:border-cyan-400 block mt-1 w-full" type="text" name="name" :value="old('name')" required
                 autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -12,7 +12,7 @@
         <!-- Date -->
         <div class="mt-4">
             <x-input-label for="data_nasc" :value="__('Data de nascimento')" />
-            <x-text-input id="data_nasc" class="block mt-1 w-full" type="date" name="data_nasc"
+            <x-text-input style="background-color: #F4F4F4;" id="data_nasc" class="focus:border-cyan-400 block mt-1 w-full" type="date" name="data_nasc"
                 :value="old('data_nasc')" required autocomplete="username" max="{{ date('Y-m-d') }}" />
             <x-input-error :messages="$errors->get('data_nasc')" class="mt-2" />
         </div>
@@ -20,7 +20,7 @@
         <!-- CPF -->
         <div class="mt-4">
             <x-input-label for="cpf" :value="__('CPF')" />
-            <x-text-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')"
+            <x-text-input style="background-color: #F4F4F4;" id="cpf" class="focus:border-cyan-400 block mt-1 w-full" type="text" name="cpf" :value="old('cpf')"
                 placeholder="000.000.000-00" required autocomplete="cpf" maxlength="14" oninput="formatarCPF(this)" />
             <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
         </div>
@@ -28,7 +28,7 @@
         <!-- Telefone -->
         <div class="mt-4">
             <x-input-label for="telefone" :value="__('Telefone')" />
-            <x-text-input id="telefone" class="block mt-1 w-full" type="text" name="telefone" :value="old('telefone')"
+            <x-text-input style="background-color: #F4F4F4;" id="telefone" class="focus:border-cyan-400 block mt-1 w-full" type="text" name="telefone" :value="old('telefone')"
                 placeholder="(00) 0000-0000" required autocomplete="telefone" maxlength="15" oninput="formatarTelefone(this)" />
             <x-input-error :messages="$errors->get('telefone')" class="mt-2" />
         </div>
@@ -36,7 +36,7 @@
         <!-- Email -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+            <x-text-input style="background-color: #F4F4F4;" id="email" class="focus:border-cyan-400 block mt-1 w-full" type="email" name="email" :value="old('email')" required
                 autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
@@ -45,7 +45,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+            <x-text-input style="background-color: #F4F4F4;" id="password" class="focus:border-cyan-400 block mt-1 w-full" type="password" name="password" required
                 autocomplete="new-password" maxlength="32" pattern="^(?=.*[A-Z])(?=.*\d).{8,}$" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
             <p class="mt-2 text-sm text-gray-600">A senha deve conter no mínimo uma letra maiúscula, um número e ter pelo menos 8 caracteres.</p>
@@ -55,7 +55,7 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+            <x-text-input style="background-color: #F4F4F4;" id="password_confirmation" class="focus:border-cyan-400 block mt-1 w-full" type="password"
                 name="password_confirmation" maxlength="32" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
@@ -65,7 +65,7 @@
             <x-input-label :value="__('Nível de acesso')" />
             <div class="flex items-center mt-2">
                 <input id="aluno" type="radio"
-                    class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" name="nivel_acesso"
+                    class="form-radio h-4 w-4 text-cyan-400 transition duration-150 ease-in-out" name="nivel_acesso"
                     value="aluno" checked>
                 <label for="aluno" class="ml-2 block text-sm leading-5 text-gray-900">
                     Aluno
@@ -73,7 +73,7 @@
             </div>
             <div class="flex items-center mt-2">
                 <input id="professor" type="radio"
-                    class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" name="nivel_acesso"
+                    class="form-radio h-4 w-4 text-cyan-400 transition duration-150 ease-in-out" name="nivel_acesso"
                     value="professor">
                 <label for="professor" class="ml-2 block text-sm leading-5 text-gray-900">
                     Professor
@@ -81,7 +81,7 @@
             </div>
             <div class="flex items-center mt-2">
                 <input id="admin" type="radio"
-                    class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" name="nivel_acesso"
+                    class="form-radio h-4 w-4 text-cyan-400 transition duration-150 ease-in-out" name="nivel_acesso"
                     value="admin">
                 <label for="admin" class="ml-2 block text-sm leading-5 text-gray-900">
                     Administrador
@@ -90,15 +90,11 @@
             <x-input-error :messages="$errors->get('tipo_usuario')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                href="{{ route('login') }}">
-                {{ __('Já tem cadastro?') }}
-            </a>
+        <x-primary-button class="mt-4" style="display: block; margin: 0 auto; background-color: #22d3ee; text-align: center; width: fit-content;">
+    {{ __('Cadastrar') }}
+</x-primary-button>
 
-            <x-primary-button class="ms-4">
-                {{ __('Cadastrar') }}
-            </x-primary-button>
+
         </div>
     </form>
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Painel de controle') }}
+                {{ __('Home') }}
             </h2>
 
             <link rel="stylesheet" href="stylefooter.css">
