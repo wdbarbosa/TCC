1aaf6a4a70410e1cb9046c06f081d3fd19d02b7d
Clara Vargas <161971153+ClaraaVargas@users.noreply.github.com>
2024-07-27 11:44:21 -0300

    CLARA - RESUMOS
    ResumoController
    rodape
    resumos
    resumosAbrir
    resumosAdiconar
    resumosEditar
    resumosForm

--- resources/views/layouts/_rodape.blade.php
+++ resources/views/layouts/_rodape.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <footer class="footer">
         <div class="container">
             <div class="row">
@@ -11,8 +12,8 @@
                 <div class="footer-col">
                     <h4>Nossas redes sociais</h4>
                     <div class="social-links">
-                        <a href="https://www.facebook.com/cursinhoprimeirodemaio/?locale=pt_BR"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://www.instagram.com/cursinhoprimeirodemaio/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/cursinhoprimeirodemaio/?locale=pt_BR"><i class="fa fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/cursinhoprimeirodemaio/"><i class="fa fa-instagram"></i></a>
                     </div>
                 </div>
                 <div class="footer-col">
@@ -28,7 +29,7 @@
                     </ul>
                 </div>
                 <div class="footer-col">
-                    <img src="./img/primeirodemaio.png" width="90" height="90">
+                    <img src="{{ asset('img/primeirodemaio.png') }}" width="90" height="90" alt="Primeiro de Maio">
                 </div>
                 <div class="footer-col">
                     <h1>&copy; Copyright 2024 SWILCOM</h1>
--- resources/views/resumos.blade.php
+++ resources/views/resumos.blade.php
@@ -6,44 +6,49 @@
             </h2>
     </x-slot>
     <!DOCTYPE html>
-       
-        <html lang="pt-br">
-            <body>
+    <html lang="pt-br">
+        <body>
+            <main>
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                             <div class="p-6 text-gray-900 dark:text-gray-100">
-                        
                                 <h2>Meus Resumos</h2>
                                 <div class="filtro">
                                     <form action="{{ route('resumo.index') }}" method="get">
                                         <p>Selecione a disciplina para busca</p>
                                         <select name="id_busca">
                                             <option value="">Todas as disciplinas</option>
-                                            @foreach($disciplina as $disciplina)
-                                                <option value="{{ $disciplina->id_disciplina }}">{{ $disciplina->descricao_disciplina }}</option>
+                                            @foreach($disciplinas as $disciplina)
+                                                <option value="{{ $disciplina->id_disciplina }}" {{ request('id_busca') == $disciplina->id_disciplina ? 'selected' : '' }}>
+                                                    {{ $disciplina->disciplina_descricao }}
+                                                </option>
                                             @endforeach
                                         </select>
                                         <button type="submit" class="botao-buscar">Buscar</button>
                                     </form>
                                 </div>
-                                <div class="lista">
-                                    @foreach($resumos as $resumo)
-                                        <div class="item">
-                                            <img src="miniatura" alt="Miniatura do resumo">
-                                            <p class="titulo">{{ $resumo->titulo }}</p>
-                                            <p class="data-publicado">Publicado em: {{ $resumo->datapublicado }}</p>
-                                            @if($resumo->dataeditado)
-                                                <p class="data-editado">Editado em: {{ $resumo->dataeditado }}</p>
-                                            @endif
-                                            <div class="acoes">
-                                                <a href="{{ route('resumo.abrir', $resumo->id_resumo) }}" class="botao botao-abrir" target="_blank">Abrir</a>
-                                                <a href="{{ route('resumo.editar', $resumo->id_resumo) }}" class="botao botao-editar">Editar</a>
-                                                <a href="{{ route('resumo.deletar', $resumo->id_resumo) }}" class="botao botao-deletar">Apagar</a>
+                                @if($resumos ->isEmpty())
+                                    <h3>Não há resumos aqui, adicione um!!</h3>
+                                @else
+                                    <div class="lista">
+                                        @foreach($resumos as $resumo)
+                                            <div class="item">
+                                                <img src="miniatura" alt="Miniatura do resumo">
+                                                <p class="titulo">{{ $resumo->titulo }}</p>
+                                                <p class="data-publicado">Publicado em: {{ $resumo->datapublicado }}</p>
+                                                @if($resumo->dataeditado)
+                                                    <p class="data-editado">Editado em: {{ $resumo->dataeditado }}</p>
+                                                @endif
+                                                <div class="acoes">
+                                                    <a href="{{ route('resumo.abrir', $resumo->id_resumo) }}" class="botao botao-abrir" target="_blank">Abrir</a>
+                                                    <a href="{{ route('resumo.editar', $resumo->id_resumo) }}" class="botao botao-editar">Editar</a>
+                                                    <a href="{{ route('resumo.deletar', $resumo->id_resumo) }}" class="botao botao-deletar">Apagar</a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    @endforeach
-                                </div>
+                                        @endforeach
+                                    </div>
+                                @endif
                                 <div class="adicionar">
                                     <a href="{{ route('resumo.adicionar') }}" class="botao-adicionar">Adicionar<a>
                                 </div>
@@ -51,7 +56,8 @@
                         </div>
                     </div>
                 </div>
-                @include('layouts._rodape')
-            </body>
+            </main>
+            @include('layouts._rodape')
+        </body>
     </html>
 </x-app-layout>
