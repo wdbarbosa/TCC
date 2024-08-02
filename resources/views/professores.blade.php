ebdcf2ad33816e524334dc5c16a784493b0f6840
maria-gbr <141789600+maria-gbr@users.noreply.github.com>
2024-08-02 08:25:41 -0300

    crud prof

--- resources/views/professores.blade.php
+++ resources/views/professores.blade.php
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylealunosblade.css">
-    <title>Professores</title>
-    
-</head>
-<body>
-    <h2>Cadastros de Professores</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Data de Nascimento</th>
-                <th>CPF</th>
-                <th>Telefone</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($user as $user): ?>
-                <?php if($user->nivel_acesso === 'professor'): ?>
-                    <tr>
-                        <td><?php echo $user->name; ?></td>
-                        <td><?php echo $user->email; ?></td>
-                        <td><?php echo $user->data_nasc; ?></td>
-                        <td><?php echo $user->cpf; ?></td>
-                        <td><?php echo $user->telefone; ?></td>
-                        <td>
-                            <a class="button" href="/editar-professor/<?php echo $user->id; ?>">Editar</a>
-                            <a class="button" href="/excluir-professor/<?php echo $user->id; ?>">Excluir</a>
-                        </td>
-                    </tr>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </tbody>
-        </table>
-        <a class="button" href="/dashboard">Voltar</a>
-    <a class="button" href="/adicionarProfessor">Adicionar</a>
-</body>
-</html>
-=======
+
 <x-app-layout>
 <x-slot name="header">
         <link rel="stylesheet" href="stylefooter.css">
@@ -82,16 +37,16 @@
                                                     <td><?php echo $user->cpf; ?></td>
                                                     <td><?php echo $user->telefone; ?></td>
                                                     <td>
-                                                        <a class="button" href="/editar-professor/<?php echo $user->id; ?>"><img width="24" height="24" src="https://img.icons8.com/material-sharp/24/FFFFFF/pencil--v1.png" alt="pencil--v1"/></a>
-                                                        <a class="button" href="/excluir-professor/<?php echo $user->id; ?>"><img width="24" height="24" src="https://img.icons8.com/material-rounded/24/FFFFFF/filled-trash.png" alt="filled-trash"/></a>
+                                                    <a class="button" href="/editar-professor/<?php echo $user->id; ?>">Editar</a>
+                                                    <a class="button" href="/excluir-professor/<?php echo $user->id; ?>">Excluir</a>
                                                     </td>
                                                 </tr>
                                             <?php endif; ?>
                                         <?php endforeach; ?>
                                     </tbody>
                                     </table>
-                                    <a class="button" href="/dashboard"><img width="22" height="22" src="https://img.icons8.com/sf-black-filled/64/FFFFFF/back.png" alt="back" alt="left2"/></a>
-                                <a class="button" href="/adicionarProfessor"><img width="21" height="21" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/plus-math.png" alt="plus-math"/></a>
+                                    <a class="button" href="/dashboard">Voltar</a>
+                                    <a class="button" href="/adicionarProfessor">Adicionar</a>
                             </div>
                         </div>
                     </div>
@@ -101,4 +56,4 @@
         </body>
     </html>
 </x-app-layout>
->>>>>>> 309ddc23ab04dc8deb73fd8ac9544a6881e144e3
+
