dd8fe08bfac896d2dfda153e4793533f0a0bbcda
maria-gbr <141789600+maria-gbr@users.noreply.github.com>
2024-08-02 08:18:17 -0300

    02/08

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,16 +24,12 @@
                         <a class="dropdown-item" href="/alterarInformacao">Alterar Informações</a>
                     </div>
                 </div>
-            @else
-
             @endif
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-
-                @foreach($turmas as $turma)
 
                 @foreach($turma as $turma)
                     <a href="{{ route('turmaEspecifica', $turma->id) }}" class="block bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 transition duration-300 ease-in-out transform hover:scale-105 turma-block">
