d0da99ce24ca8d21d484a979484f4d947abc4bde
SofiaAyumi <sofiayumi17@gmail.com>
2024-08-05 12:11:02 -0300

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,32 +4,41 @@
         <div class="flex justify-between h-16">
             <div class="flex" style="width:100%;">
 
+                <!-- Botão Hambúrguer -->
+                <div class="-me-2 flex items-center lg:hidden">
+                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 lg:-my-px lg:ms-10 lg:flex" style="justify-content: space-between; width: 100%;">
-                    <div class="shrink-0 flex items-center" >
+                    <div class="shrink-0 flex items-center">
                         <a href="{{ route('dashboard') }}" class="pr-2">
                             <x-application-logo/>
                         </a>
-                        <div class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:lg:col-start-2 " >
-                                @if (Route::has('login'))
-                                    <nav class="-mx-3 flex flex-1 justify-end">
-                                @auth
-                                     <a href="{{ url('/dashboard') }}">
-                            </a>
-                        @else
-                            <a href="{{ route('login') }}">
-                            <u>Login</u>
-                                @if (Route::has('register'))                                 
-                            </a>
-                                @endif
-                        @endauth
-                    </nav>
-            @endif
-        </div>
-    </div>  
                     </div>
-                    
+
+ <!-- Bloco de Login -->
+<div class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+    <div class="flex lg:col-start-2">
+        @if (Route::has('login'))
+            <nav class="-mx-3 flex flex-1 justify-end">
+                @auth
+                    <!-- Nada é exibido aqui se estiver autenticado -->
+                @else
+                    <a href="{{ route('login') }}" class="text-black">
+                        <u>Login</u>
+                    </a>
+                @endauth
+            </nav>
+        @endif
+    </div>
+</div>                   
+
                     @auth
                         <div class="hidden space-x-8 lg:-my-px lg:ms-10 lg:flex">
                             @if(auth()->user()->nivel_acesso === 'admin' || auth()->user()->nivel_acesso === 'aluno' || auth()->user()->nivel_acesso === 'professor')
@@ -81,7 +90,6 @@
                                 <x-slot name="trigger">
                                     <button class="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:#9dc8ce focus:outline-none transition ease-in-out duration-150">
                                         <div>{{ Auth::user()->name }}</div>
-
                                         <div class="ms-1">
                                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -95,10 +103,8 @@
                                         {{ __('Perfil') }}
                                     </x-dropdown-link>
 
-                                    <!-- Authentication -->
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
-
                                         <x-dropdown-link :href="route('logout')"
                                                 onclick="event.preventDefault();
                                                             this.closest('form').submit();">
@@ -109,78 +115,89 @@
                             </x-dropdown>
                         </div>
                     @endauth
-
-                    <!-- Hamburger -->
-                    <div class="-me-2 flex items-center lg:hidden">
-                        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
                 </div>
             </div>
+        </div>
 
-            <!-- Responsive Navigation Menu -->
-            <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
-                <div class="pt-1 pb-3 space-y-1">
-                    @auth
+        <!-- Menu Responsivo -->
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
+            <div class="pt-1 pb-3 space-y-1">
+                @auth
+                    @if(auth()->user()->nivel_acesso === 'admin' || auth()->user()->nivel_acesso === 'aluno' || auth()->user()->nivel_acesso === 'professor')
                         <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
+                            {{ __('Home') }}
                         </x-responsive-nav-link>
+                    @endif
+
+                    @if(auth()->user()->nivel_acesso === 'professor' || auth()->user()->nivel_acesso === 'aluno')
                         <x-responsive-nav-link :href="route('forumdeduvidas')" :active="request()->routeIs('forumdeduvidas')">
                             {{ __('Fórum de Dúvidas') }}
                         </x-responsive-nav-link>
+                    @endif
+
+                    @if(auth()->user()->nivel_acesso === 'professor' || auth()->user()->nivel_acesso === 'aluno')
                         <x-responsive-nav-link :href="route('disciplinas')" :active="request()->routeIs('disciplinas')">
                             {{ __('Disciplinas') }}
                         </x-responsive-nav-link>
+                    @endif
+
+                    @if(auth()->user()->nivel_acesso === 'professor' || auth()->user()->nivel_acesso === 'aluno')
                         <x-responsive-nav-link :href="route('questoes.index')" :active="request()->routeIs('questoes.index')">
                             {{ __('Questões') }}
                         </x-responsive-nav-link>
+                    @endif
+
+                    @if(auth()->user()->nivel_acesso === 'aluno')
                         <x-responsive-nav-link :href="route('resumo.index')" :active="request()->routeIs('resumo.index')">
                             {{ __('Resumos') }}
                         </x-responsive-nav-link>
+                    @endif
+
+                    @if(auth()->user()->nivel_acesso === 'admin' || auth()->user()->nivel_acesso === 'aluno' || auth()->user()->nivel_acesso === 'professor')
                         <x-responsive-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
                             {{ __('Informações') }}
                         </x-responsive-nav-link>
+                    @endif
+
+                    @if(auth()->user()->nivel_acesso === 'professor' || auth()->user()->nivel_acesso === 'aluno')
                         <x-responsive-nav-link :href="route('comunicados')" :active="request()->routeIs('comunicados')">
                             {{ __('Comunicados') }}
                         </x-responsive-nav-link>
-                        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-                            <div class="px-4">
-                                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                                <div class="font-medium text-lg text-gray-500">{{ Auth::user()->email }}</div>
-                            </div>
+                    @endif
 
-                            <div class="mt-3 space-y-1">
-                                <x-responsive-nav-link :href="route('profile.edit')">
-                                    {{ __('Perfil') }}
+                    <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+                        <div class="px-4">
+                            <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                            <div class="font-medium text-lg text-gray-500">{{ Auth::user()->email }}</div>
+                        </div>
+
+                        <div class="mt-3 space-y-1">
+                            <x-responsive-nav-link :href="route('profile.edit')">
+                                {{ __('Perfil') }}
+                            </x-responsive-nav-link>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-responsive-nav-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Sair') }}
                                 </x-responsive-nav-link>
-
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-
-                                    <x-responsive-nav-link :href="route('logout')"
-                                            onclick="event.preventDefault();
-                                                        this.closest('form').submit();">
-                                        {{ __('Sair') }}
-                                    </x-responsive-nav-link>
-                                </form>
-                            </div>
+                            </form>
                         </div>
-                    @endauth
+                    </div>
+                @endauth
 
-                    @guest
-                        <div class="shrink-0 flex items-center">
-                            <a href="{{ route('dashboard') }}" class="pr-2">
-                                <x-application-logo/>
-                            </a>
-                        </div>
-                    @endguest
-                </div>
+                @guest
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('dashboard') }}" class="pr-2">
+                            <x-application-logo/>
+                        </a>
+                    </div>
+                @endguest
             </div>
         </div>
     </div>
 </nav>
+
+
