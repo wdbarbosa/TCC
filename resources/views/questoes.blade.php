8f0d3d4f8d51a248bfd89d6b4a1dee18af0ae352
isabelaxavierr <isabela.xavier@unesp.br>
2024-08-10 12:00:49 -0300

    update JS em questoes

--- resources/views/questoes.blade.php
+++ resources/views/questoes.blade.php
@@ -12,7 +12,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                 <!--@if(session('success'))
+                    @if(session('success'))
                         <script>
                             alert('{{ session('success') }}');
                         </script>
@@ -21,7 +21,7 @@
                         <script>
                             alert('{{ session('deletado') }}');
                         </script>
-                    @endif -->
+                    @endif
                     <a href="{{ route('questoes.criar') }}">Adicionar Questão</a>
                         <ul>
                             @foreach($questoes as $questao)
