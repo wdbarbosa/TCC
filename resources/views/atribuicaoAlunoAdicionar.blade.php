4ab815645825119edd75b535a6d13f68b15abd20
Clara Vargas <161971153+ClaraaVargas@users.noreply.github.com>
2024-08-12 11:28:31 -0300

    atribuição - segunda

--- resources/views/atribuicaoAlunoAdicionar.blade.php
+++ resources/views/atribuicaoAlunoAdicionar.blade.php
@@ -17,21 +17,7 @@
                     @else
                         <form action="{{ route('atribuicaoaluno.salvar') }}" method="POST">
                             {{ csrf_field() }}
-                            <table>
-                                @foreach($alunos as $aluno)
-                                    <tr>
-                                        <td>{{ $aluno->user->name }}</td>
-                                        <td>
-                                            <select name="turma[{{ $aluno->fk_aluno_users_id }}]" required>
-                                                <option value="" disabled>Selecione uma turma</option>
-                                                @foreach($turmas as $turma)
-                                                    <option value="{{ $turma->id }}">{{ $turma->nome }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </table>
+                            
                             <button type="submit">Salvar</button>
                         </form>
                     @endif
