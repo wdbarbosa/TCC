5c73531bd0cfebb6d22429596582b13b750c334e
SofiaAyumi <sofiayumi17@gmail.com>
2024-08-16 08:05:22 -0300

    cabecalho

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,6 @@
 <nav x-data="{ open: false }" style="background-color: #9dc8ce;">
+<link rel="stylesheet" href="stylenav.css">
+
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 lg:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,7 +8,7 @@
 
                 <!-- Botão Hambúrguer -->
                 <div class="-me-2 flex items-center lg:hidden">
-                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                    <button @click="open = ! open" class="menu-button inline-flex items-center justify-center rounded-md text-gray-400 dark:text-gray-500 focus:outline-none transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                             <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -21,27 +23,27 @@
                             <x-application-logo/>
                         </a>
                     </div>
-<!-- Bloco de Login -->
-<div class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-    <div class="flex lg:col-start-2">
-        @if (Route::has('login'))
-            <nav class="-mx-10 flex flex-1 justify-end">
-                @auth
-                    <!-- Nada é exibido aqui se estiver autenticado -->
-                @else
-                    <a href="{{ route('login') }}" class="bg-white text-black py-1 px-3 rounded-lg text-base font-medium hover:bg-gray-200 transition -mt-4">
-                        Login
-                    </a>
-                    <a href="{{ route('register') }}" class="bg-white text-black py-1 px-3 rounded-lg text-base font-medium hover:bg-gray-200 transition -mt-4">
-                        Cadastro
-                    </a>
-                @endauth
-            </nav>
-        @endif
-    </div>
-</div>
+                    <!-- Bloco de Login -->
+                    <div class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                        <div class="flex lg:col-start-2">
+                            @if (Route::has('login'))
+                                <nav class="-mx-10 flex flex-1 justify-end">
+                                    @auth
+                                        <!-- Nada é exibido aqui se estiver autenticado -->
+                                    @else
+                                        <a href="{{ route('login') }}" class="bg-white text-black py-1 px-3 rounded-lg text-base font-medium hover:bg-gray-200 transition -mt-4">
+                                            Login
+                                        </a>
+                                        <a href="{{ route('register') }}" class="bg-white text-black py-1 px-3 rounded-lg text-base font-medium hover:bg-gray-200 transition -mt-4">
+                                            Cadastro
+                                        </a>
+                                    @endauth
+                                </nav>
+                            @endif
+                        </div>
+                    </div>
 
-          @auth
+                    @auth
                         <div class="hidden space-x-8 lg:-my-px lg:ms-10 lg:flex">
                             @if(auth()->user()->nivel_acesso === 'admin' || auth()->user()->nivel_acesso === 'aluno' || auth()->user()->nivel_acesso === 'professor')
                                 <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-white">
@@ -80,19 +82,19 @@
                             @endif
                         </div>
 
-                     <!-- Settings Dropdown -->
-<div class="hidden lg:flex lg:items-center lg:ms-6">
-    <x-dropdown width="48">
-        <x-slot name="trigger">
-            <button class="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:bg-gray-100 focus:outline-none transition ease-in-out duration-150">
-                <div>{{ Auth::user()->name }}</div>
-                <div class="ms-1">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                </div>
-            </button>
-        </x-slot>
+                        <!-- Settings Dropdown -->
+                        <div class="hidden lg:flex lg:items-center lg:ms-6">
+                            <x-dropdown width="48">
+                                <x-slot name="trigger">
+                                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:bg-gray-100 focus:outline-none transition ease-in-out duration-150">
+                                        <div>{{ Auth::user()->name }}</div>
+                                        <div class="ms-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
 
                                 <x-slot name="content">
                                     <x-dropdown-link :href="route('profile.edit')">
