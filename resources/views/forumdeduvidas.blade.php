b24430673f0d674037eb75b9ea693673a20f75f9
ana <wendel.rafael@unesp.br>
2024-08-17 10:14:55 -0300

    css

--- resources/views/forumdeduvidas.blade.php
+++ resources/views/forumdeduvidas.blade.php
@@ -1,18 +1,17 @@
 <x-app-layout>
     @section('title', 'Cursinho Primeiro de Maio')
     <x-slot name="header">
-    <link rel="stylesheet" href="{{ asset('stylefooter.css')}}">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Fórum de Dúvidas') }}
         </h2>
     </x-slot>
+    <link rel="stylesheet" href="{{ asset('stylefooter.css')}}">
 
     @if(auth()->user()->nivel_acesso === 'aluno')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-
                         <a class="dropdown-item" href="/adicionarDuvida">Adicionar duvida</a>
 
                 </div>
@@ -73,6 +72,9 @@
             @endforelse
         </div>
     </div>
+@include('layouts._rodape')
+
+
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             // Seleciona todos os links "Responder" pela classe
@@ -94,6 +96,4 @@
             });
         });
     </script>
-
-    @include('layouts._rodape')
 </x-app-layout>
