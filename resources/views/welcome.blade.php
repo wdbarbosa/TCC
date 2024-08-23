63c7c32245359135d9bbefdfc202655796b80392
lais <laisacquintao@gmail.com>
2024-08-23 08:55:14 -0300

    terceira vez seguidaaaaaaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,29 +12,6 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Informações') }}
         </h2>
-
-        @if(auth()->user()->nivel_acesso === 'admin')
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ação do Administrador
-                    </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="/professor">Gerenciar Professores</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/aluno">Gerenciar Alunos</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/turma">Gerenciar Turmas</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/disciplina">Gerenciar Disciplinas</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/alterarInformacao">Alterar Informações</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('atribuicaoprofessor.index') }}">Atribuir Professores</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('atribuicaoaluno.index') }}">Atribuir Alunos</a>
-                        </div>
-                </div>
-            @endif
     </x-slot>
 
     <!DOCTYPE html>
