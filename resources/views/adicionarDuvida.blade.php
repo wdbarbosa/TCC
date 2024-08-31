e29a57e9e530a0017889824cad0373a8d241aecb
SofiaAyumi <sofiayumi17@gmail.com>
2024-08-31 11:49:09 -0300

    front

--- resources/views/adicionarDuvida.blade.php
+++ resources/views/adicionarDuvida.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="styletitulo.css">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Criação de Dúvidas
+                Fórum de Dúvidas
             </h2>
         </div>
     </x-slot>
