0bcd6a68175085b5a5c614a841b3593e3978ab08
SofiaAyumi <sofiayumi17@gmail.com>
2024-09-03 10:12:40 -0300

    reajuste

--- resources/views/disciplinas.blade.php
+++ resources/views/disciplinas.blade.php
@@ -11,16 +11,15 @@
     <link rel="stylesheet" href="styledisciplinas.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 <main>
-    <div class="grid-container py-12">
-                    @foreach($disciplina as $disciplina)
-                     <a href="{{ route('disciplinaEspecifica', $disciplina->id) }}" 
-                    class="block bg-white dark:bg-gray-800 overflow-hidden gap-100 shadow-sm sm:rounded-lg p-6 transition duration-300 ease-in-out transform hover:scale-105 turma-block"
-                    style="border: 3px solid #9dc8ce; border-radius: 10px; color: #333; margin:5%">
-                     <h3 class="text-lg font-semibold">{{ $disciplina->nome_disciplina }}</h3>
-                     <p class="text-gray-600 dark:text-gray-300">{{ $disciplina->disciplina_descricao }}</p>
-                    </a>
-                @endforeach
+    <div class="grid-container py-9">
+        @foreach($disciplina as $disciplina)
+            <a href="{{ route('disciplinaEspecifica', $disciplina->id) }}" 
+                class="turma-block">
+                <h3 class="text-lg font-semibold">{{ $disciplina->nome_disciplina }}</h3>
+                <p class="text-gray-600 dark:text-gray-300">{{ $disciplina->disciplina_descricao }}</p>
+            </a>
+        @endforeach
     </div>
 </main>
-     @include('layouts._rodape')
+@include('layouts._rodape')
 </x-app-layout>
