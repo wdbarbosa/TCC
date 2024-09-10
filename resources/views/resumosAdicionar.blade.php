caaa12b5ebd9ec3d020c64f7525d264bef95a744
lais <laisacquintao@gmail.com>
2024-09-10 09:11:55 -0300

    setas nas telas de resumos 10/09

--- resources/views/resumosAdicionar.blade.php
+++ resources/views/resumosAdicionar.blade.php
@@ -3,7 +3,10 @@
 <link rel="stylesheet" href="{{ asset('styleresumos.css') }}">
 <link rel="stylesheet" href="{{ asset('stylefooter.css') }}">
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex items-center">
+            <a href="{{ route('resumo.index') }}" class="mr-4" alt="Voltar">
+                <img src="{{ asset('img/voltar.png') }}" alt="Voltar" class="w-6 h-6 hover:scale-125">
+            </a>
             {{ __('Resumos') }}
         </h2>
     </x-slot>
