0b687a86caab85ef9a43e491392e651d9f3db90f
SofiaAyumi <sofiayumi17@gmail.com>
2024-09-23 11:34:02 -0300

    front gerenciar turmas

--- resources/views/turmas.blade.php
+++ resources/views/turmas.blade.php
@@ -1,54 +1,66 @@
 <x-app-layout>
-    @section('title', 'Cursinho Primeiro de Maio')
-    <x-slot name="header">
-        <link rel="stylesheet" href="{{ asset('/stylefooter.css') }}">
-        <link rel="stylesheet" href="{{ asset('/stylecrudturmas.css') }}">
-        <link rel="stylesheet" href="{{ asset('/stylefuncaoadmin.css') }}">
-        <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex items-center">
-                <a href="{{ route('dashboard') }}" class="mr-4" alt="Voltar">
-                    <img src="{{ asset('img/voltar.png') }}" alt="Voltar" class="w-6 h-6 hover:scale-125">
-                </a>
-                {{ __('Turmas') }}
-            </h2>
-            @include('layouts._funcaoadmin')
+@section('title', 'Cursinho Primeiro de Maio')
+
+<x-slot name="header">
+    <link rel="stylesheet" href="{{ asset('/stylefooter.css') }}">
+    <link rel="stylesheet" href="{{ asset('/stylecrudturmas.css') }}">
+    <link rel="stylesheet" href="{{ asset('/stylefuncaoadmin.css') }}">
+    <div class="flex justify-between items-center">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex items-center">
+            <a href="{{ route('dashboard') }}" class="mr-4" alt="Voltar">
+                <img src="{{ asset('img/voltar.png') }}" alt="Voltar" class="w-6 h-6 hover:scale-125">
+            </a>
+            {{ __('Turmas') }}
+        </h2>
+        @include('layouts._funcaoadmin')
+    </div>
+</x-slot>
+
+<main class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                <h2 class="text-lg font-semibold mb-4 text-center">Cadastros de Turmas</h2>
+                <div class="table-container">
+                    <table class="w-full">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($turmas as $turma)
+                                <tr>
+                                    <td class="text-center">{{ $turma->nome }}</td>
+                                    <td class="descricao text-center">{{ $turma->descricao }}</td>
+                                    <td class="text-center">
+                                        <a class="button" href="{{ route('turma.edit', $turma->id) }}">Editar</a>
+                                        <a class="button" href="{{ route('turma.destroy', $turma->id) }}" 
+                                            onclick="event.preventDefault(); document.getElementById('delete-form-{{ $turma->id }}').submit();">
+                                            Excluir
+                                        </a>
+                                        <form id="delete-form-{{ $turma->id }}" action="{{ route('turma.destroy', $turma->id) }}" method="POST" style="display: none;">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="button-container">
+                    <a class="button" href="{{ route('turma.create') }}">Adicionar</a>
+                </div>
+            </div>
         </div>
-    </x-slot>
-    <main>
-        <h2>Cadastros de Turmas</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Descrição</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($turmas as $turma)
-                        <tr>
-                            <td>{{ $turma->nome }}</td>
-                            <td class="descricao">{{ $turma->descricao }}</td>
-                            <td>
-                                <a class="button" href="{{ route('turma.edit', $turma->id) }}">Editar</a>
-                                <a class="button" href="{{ route('turma.destroy', $turma->id) }}" 
-                                    onclick="event.preventDefault(); document.getElementById('delete-form-{{ $turma->id }}').submit();">
-                                    Excluir
-                                </a>
-                                <form id="delete-form-{{ $turma->id }}" action="{{ route('turma.destroy', $turma->id) }}" method="POST" style="display: none;">
-                                    @csrf
-                                    @method('DELETE')
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <a class="button" href="{{ route('dashboard') }}">Voltar</a>
-            <a class="button" href="{{ route('turma.create') }}">Adicionar</a>
-        </main>    
-    <footer>
+    </div>
+</main>
+
+
+<footer>
     @include('layouts._rodape')
-    </footer>
-   
+</footer>
 </x-app-layout>
