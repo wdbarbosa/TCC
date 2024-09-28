ac66bafa668473eecf3fb1319fe57094ea52a9d1
lais <laisacquintao@gmail.com>
2024-09-28 11:57:42 -0300

    erro merge commit anterior

--- resources/views/materialDidatico.blade.php
+++ resources/views/materialDidatico.blade.php
@@ -59,13 +59,15 @@
 
                                 
                                     <div class="botao-container">
-                                        <a class="editar-btn" href="{{ route('materiais.editar', [$disciplina->id, $material->id, $turma->id]) }}">
-                                            Editar
-                                        </a>
+                                    <a class="editar-btn" href="{{ route('materiais.editar', [$disciplina->id, $material->id, $turma->id]) }}">
+                                        <img src="{{ asset('img/escrever.png') }}" alt="Editar" class="icon-escrever">
+                                    </a>
                                         <form action="{{ route('materiais.deletar', [$disciplina->id, $material->id, $turma->id]) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="deletar-btn">Excluir</button>
+                                            <button type="submit" class="deletar-btn">
+                                                <img src="{{ asset('img/lixeira-de-reciclagem.png') }}" alt="Excluir" class="icon-lixeira">
+                                            </button>
                                         </form>
                                     </div>
                                 </li>
