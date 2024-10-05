afe1115ed53c2cfcf6fb844dceecb1c0ae9e3945
lais <laisacquintao@gmail.com>
2024-10-05 10:24:55 -0300

    espacamento forum duvidas 05/10

--- resources/views/forumdeduvidas.blade.php
+++ resources/views/forumdeduvidas.blade.php
@@ -10,7 +10,7 @@
     </x-slot>
 
     @if(auth()->user()->nivel_acesso === 'aluno')
-        <div class="py-12">
+        <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg"> 
                     <div class="p-6 text-gray-900 dark:text-gray-100">
