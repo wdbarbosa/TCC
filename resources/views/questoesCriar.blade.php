1238a104d5fc7bb782280b84367ad9c6bede4d29
isabelaxavierr <isabela.xavier@unesp.br>
2024-10-25 09:18:14 -0300

    Update enunciado questoes

--- resources/views/questoesCriar.blade.php
+++ resources/views/questoesCriar.blade.php
@@ -31,7 +31,7 @@
 
                             <div class="flex flex-col">
                                 <label for="enunciado" class="font-medium text-gray-700">Enunciado:</label>
-                                <input type="text" name="enunciado" id="enunciado" class="mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
+                                <textarea name="enunciado" id="enunciado" rows="4" style="resize: none;" class="mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required></textarea>
                             </div>
 
                             <div class="flex flex-col">
@@ -94,7 +94,9 @@
                                 </select>
                             </div>
 
-                            <button type="submit" class="mt-4 bg-[#6bb6c0] text-white py-2 px-4 rounded hover:bg-[#8ab3b6] transition duration-150">Salvar</button>
+                            <div class="flex justify-center">
+                                <button type="submit" class="mt-4 bg-[#6bb6c0] text-white py-2 px-4 rounded hover:bg-[#8ab3b6] transition duration-150">Salvar</button>
+                            </div>
                         </form>
                     </div>
                 </div>
