fd2ee7f9bfb1068aab73968254bc683914d06b1f
Isabela Xavier <isabelaaureliano10@gmail.com>
2024-11-22 22:21:28 -0300

    para o aluno ver direito

--- resources/views/disciplinaEspecifica.blade.php
+++ resources/views/disciplinaEspecifica.blade.php
@@ -53,11 +53,11 @@
                                     @endif
 
                                     @if($material->pdf)
-                                    <p>PDF: <a href="{{ asset('storage/'.$material->pdf) }}" target="_blank"><u>Ver PDF</u></a></p>
+                                    <p>PDF: <a href="{{ asset('TCC/public/' . $material->pdf) }}" target="_blank"><u>Ver PDF</u></a></p>
                                     @endif
 
                                     @if($material->slide)
-                                    <p>Slide: <a href="{{ asset('storage/'.$material->slide) }}" target="_blank"><u>Ver Slide</u></a></p>
+                                    <p>Slide: <a href="{{ asset('TCC/public/' . $material->slide) }}" target="_blank"><u>Ver Slide</u></a></p>
                                     @endif
                                 </div>
                             @endforeach
